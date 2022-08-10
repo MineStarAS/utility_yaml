5f5dbe8980865c85ac6377e3e3052ba3e8a79c48
MineStar <band1019@naver.com>
2022-08-11 00:17:00 +0900

    #

--- my_flutter_sdk/example/android/app/src/main/kotlin/dev/tykan/my_flutter_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.tykan.my_flutter_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- my_flutter_sdk/lib/my_flutter_sdk.dart
+++
@@ -1,8 +0,0 @@
-
-import 'my_flutter_sdk_platform_interface.dart';
-
-class MyFlutterSdk {
-  Future<String?> getPlatformVersion() {
-    return MyFlutterSdkPlatform.instance.getPlatformVersion();
-  }
-}
