cf21d8ec61b68eaa4cd3e28dea629989a319df20
MineStar <band1019@naver.com>
2022-08-11 06:10:37 +0900

    # example.dart - add

---
+++ example/example.dart
@@ -0,0 +1,15 @@
+import 'package:utility_yaml/utility_yaml.dart';
+
+main() {
+  final yaml = YamlConfiguration.loadPath('example/config.yml');
+  print(yaml.getString('string'));
+  print(yaml.getInt('int'));
+  print(yaml.getDouble('double'));
+  print(yaml.getBoolean('boolean'));
+  print(yaml.getList('list'));
+  print(yaml.getMap('map'));
+
+  yaml.put('copy', [1, 2, 3, 4, 5]);
+
+  yaml.saveToPath('example/configCopy.yml');
+}
